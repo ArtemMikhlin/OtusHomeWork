10816364a821b38c00dcebce197ef81aea1f6e7f
ArtemMikhlin <46351445+ArtemMikhlin@users.noreply.github.com>
2024-10-21 21:17:20 +0300

    SecondTask
    Определить сущности геометрических фигур (например треугольник, квадрат, круг и тд), реализовать функцию описания у фигур, реализовать общий класс фигура - который содержит в себе описание, количество углов, функцию draw() (рисовать не нужно, можно просто вывести в консоль что-нибудь) - переопределить соотвествующие методы у сабклассов.

--- ClassHomeWork.playground/Contents.swift
+++ ClassHomeWork.playground/Contents.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+//Task1
 class Calculate {
     var num1: Int = 0
     var num2: Int = 0
@@ -9,23 +10,80 @@ class Calculate {
         self.num2 = num2
     }
     
-    func sum() -> Int{
+    func sum() -> Int {
         num1 + num2
     }
     
-    func difference() -> Int{
+    func difference() -> Int {
         num1 - num2
     }
     
-    func product() -> Int{
+    func product() -> Int {
         num1 * num2
     }
     
-    func quotient() -> Any{
+    func quotient() -> Any {
         if num2 != 0{return num1 / num2}
         else {return("Fatal error: Division by zero")}
     }
 
+}
+
+//Task2
+class Figure {
+    var description: String {
+        return "It's figure"
+    }
+    
+    var numberOfAngles: Int {
+        return 0
+    }
+    
+    func draw() {
+        print("Draw")
+    }
+}
+
+class Triangle: Figure {
+    override var description: String {
+        return "It's triangle"
+    }
+    
+    override var numberOfAngles: Int {
+        return 3
+    }
+    
+    override func draw() {
+        print("Draw triangle.")
+    }
+}
+
+class Square: Figure {
+    override var description: String {
+        return "It's square"
+    }
+    
+    override var numberOfAngles: Int {
+        return 4
+    }
+    
+    override func draw() {
+        print("Draw square.")
+    }
+}
+
+class Circle: Figure {
+    override var description: String {
+        return "It's circle"
+    }
+    
+    override var numberOfAngles: Int {
+        return 0
+    }
+    
+    override func draw() {
+        print("Draw circle.")
+    }
 }
 
 
